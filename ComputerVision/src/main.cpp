292be5cd6d44db05a5ff897ae12225c9799bc456
RiccardoZappa <rzappa00@gmail.com>
2025-04-15 00:38:36 +0200

    modified main to ensure the correct linking of onnxruntime library

--- ComputerVision/src/main.cpp
+++ ComputerVision/src/main.cpp
@@ -1,7 +1,13 @@
 #include <opencv2/opencv.hpp>
+#include <onnxruntime_cxx_api.h>
 
 int main() {
     cv::Mat materis;
     materis = cv::imread("data/sample.jpg", cv::IMREAD_COLOR );
+    auto providers = Ort::GetAvailableProviders();
+    for (auto provider : providers)
+    {
+        std::cout << provider << std::endl;
+    }
     return 0;
 }
