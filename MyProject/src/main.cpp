88328daa7848bad69a320ab2178d51ddde06b64d
RiccardoZappa <rzappa00@gmail.com>
2025-06-30 01:06:59 +0300

    tested the index out o range exception correctly

--- MyProject/src/main.cpp
+++ MyProject/src/main.cpp
@@ -4,6 +4,15 @@
 int main()
 {
     std::cout << "Hello world!" << std::endl;
-     
+    try{
+        Tensor<int, 3, 3, 3> MyTensor(0);
+        int element =  MyTensor(3,0,0);
+
+    }
+    catch(std::out_of_range& exception)
+    {
+        std::cout << "caught an exception!!"<< std::endl;
+        std::cout << "Error: " << exception.what() << std::endl;
+    }
     return 0;
 }
