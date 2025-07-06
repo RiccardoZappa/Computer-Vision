49d17093ae038d98bfa700709ec0623bb1533ed8
RiccardoZappa <rzappa00@gmail.com>
2025-07-06 23:12:50 +0300

    fixing the test since before there was an index out of bound exception

--- MyProject/test/test.cpp
+++ MyProject/test/test.cpp
@@ -4,6 +4,6 @@
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
     Tensor<int, 3, 3, 3> MyTensor(0);
-    int element =  MyTensor(3,0,0);
-    ASSERT_EQ(0, MyTensor(2,0,0));
+    int element =  MyTensor(2,0,0);
+    ASSERT_EQ(0, element);
 }
